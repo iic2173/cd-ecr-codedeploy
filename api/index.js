a10b41b1a5d823e7400d70fc5677bba677a8fec3
domingo2000 <domingo.edwards@uc.cl>
2023-09-25 17:22:50 -0300

    feature: basic app with build and push to ecr workflow

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// GET endpoint
+app.get("/", (req, res) => {
+  const response = {
+    message: "GET request received",
+  };
+  res.json(response);
+});
+
+// POST endpoint
+app.post("/", (req, res) => {
+  const requestBody = req.body;
+  const response = {
+    message: "POST request received",
+    dataReceived: requestBody,
+  };
+  res.json(response);
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
